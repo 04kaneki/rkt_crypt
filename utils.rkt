6465b834ce365b5d673cce147d3ec04616e4bd92
04kaneki <94925493+04kaneki@users.noreply.github.com>
2022-05-11 19:05:25 +0200

    converting methods
    added methods for converting between strings and their decimal value

--- utils.rkt
+++ utils.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 ; export function(s)
-(provide gcd egcd ++ -- modulo2)
+(provide gcd egcd ++ -- modulo2 string->int int->string)
 
 ; get greatest common divisor using Euclid's algorithm
 (define (gcd a b)
@@ -34,6 +34,33 @@
 (define (-- integer)
   (- integer 1)
   )
+
+; convert string to number
+(define (string->int s)
+
+   (for/fold ((rv 0)) ((b (string->bytes/utf-8 s))) (+ b (* rv 256)))
+   ;(for/fold ((r 1)) ((i '(1 2 3 4 5))) (* r i))
+  
+  )
+
+(define (int->string N)
+  (define (inr n b)
+    (if (zero? n)
+        b
+        (inr (quotient n 256) (bytes-append (bytes (modulo n 256)) b))
+        )
+    )
+  (bytes->string/utf-8 (inr N (bytes)))
+  )
+
+;(define (test s)
+;  (let rec([B (string->bytes/utf-8 s)] [value 0] [i 0])
+;    (if (= i (-- (string-length s)))
+;           value 
+;    (rec B (+ (bytes-ref B i) (* value 256)) (++ i)))
+;    
+;    )
+;  )
 
 
 
