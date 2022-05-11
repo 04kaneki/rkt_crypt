2e8bfe5f980322b512a2b4de7a057054a931fe46
04kaneki <94925493+04kaneki@users.noreply.github.com>
2022-05-11 14:22:52 +0200

    added extended euclidean algorithm

--- utils.rkt
+++ utils.rkt
@@ -1,13 +1,22 @@
 #lang racket
 
 ; export function(s)
-(provide gcd ++ -- modulo2)
+(provide gcd egcd ++ -- modulo2)
 
 ; get greatest common divisor using Euclid's algorithm
 (define (gcd a b)
   (if (zero? a)
       b
       (gcd (modulo b a) a)
+      )
+  )
+
+; extended euclidean algorithm
+(define (egcd a b x y u v)
+
+  (if (zero? a)
+      x
+      (egcd (modulo b a) a u v (- x (* u (floor (/ b a)))) (- y (* v (floor (/ b a)))))
       )
   )
 
